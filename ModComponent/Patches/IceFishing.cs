6befe1d8b777431f8d2ce4f54cfcc7a586fdf7f7
STBlade <dommrogers@gmail.com>
2024-01-21 19:46:00 +0000

    fix MC tools useable for FishingHole ice breaking

---
+++ ModComponent/Patches/IceFishing.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using ModComponent.AssetLoader;
+
+namespace ModComponent.Patches;
+
+[HarmonyPatch(typeof(Panel_IceFishingHoleClear), nameof(Panel_IceFishingHoleClear.Launch))]
+internal static class Panel_IceFishingHoleClear_Launch
+{
+	static bool callOnce = true;
+	internal static void Prefix(Panel_IceFishingHoleClear __instance)
+	{
+		if (callOnce) {
+			__instance.AddAlternateTools();
+			callOnce = false;
+		}
+	}
+}
+
+[HarmonyPatch(typeof(Panel_IceFishingHoleClear), nameof(Panel_IceFishingHoleClear.CanBuildFishingHole))]
+internal static class Panel_IceFishingHoleClear_CanBuildFishingHole
+{
+	static bool callOnce = true;
+	internal static void Prefix(Panel_IceFishingHoleClear __instance)
+	{
+		if (callOnce)
+		{
+			__instance.AddAlternateTools();
+			callOnce = false;
+		}
+	}
+}
